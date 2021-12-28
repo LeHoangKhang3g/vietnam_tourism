08a9379890f1590d6f32767c09e07a40b27c238b
TranQuocTrung-CDTH19PMC <83913495+Trung0306191288@users.noreply.github.com>
2021-12-28 14:35:40 +0700

    update

--- vietnam_tourism_flutter/lib/places/list_place.dart
+++ vietnam_tourism_flutter/lib/places/list_place.dart
@@ -13,7 +13,7 @@ class _PlaceScreenState extends State<PlaceScreen>{
   List<Place> places = [
     Place("Nha Trang","Miền Trung", "Khu Vực Trung Bộ" , "Khám phá ngay trung tâm thành phố Nha Trang có gì chơi:  " , "place1.jpg"),
     Place("Thừa Thiên Huế","Miền Trung","Khu Vực Trung Bộ","kiến trúc thời phong kiến , phong cảnh phong tục văn hóa dân tộc việt nam" , "place2.jpg"),
-    Place("Vịnh Hạ Long" , "Miền Bắc" , "Khu Vực Bắc Bộ", "Có Nhiều bãi đẹp ,  du lịch 5 sao , phong phú về địa hình , các hòn đảo , đá san hô" , "place3.jpg"),
+    Place("Vịnh Hạ Long" , "Miền Bắc" , "Khu Vực Bắc Bộ", "Có Nhiều bãi đẹp ,  du lịch 5 sao , khu nghỉ dương vip , phong phú về địa hình , các hòn đảo , đá san hô" , "place3.jpg"),
 
 
   ];
